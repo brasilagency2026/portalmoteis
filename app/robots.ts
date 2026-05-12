c7d06e55f95df845fbbd1600d5997c85260b396f
gleveque27 <glwebagency2@gmail.com>
2026-05-12 17:56:40 -0300

    Allow facebookexternalhit in robots configuration

--- app/robots.ts
+++ app/robots.ts
@@ -4,10 +4,16 @@ const appBaseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://moteis.bdsmbrazil
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
     sitemap: `${appBaseUrl}/sitemap.xml`,
     host: appBaseUrl,
   }
